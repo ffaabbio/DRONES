0825f06be2efa7868361306dc4db9345cf05255b
Andras <andrasschuller@gmail.com>
2025-11-21 11:58:48 +0100

    exercise 3 finished version

--- src/main/java/drones/gr1/InMemoryAllMissions.java
+++ src/main/java/drones/gr1/InMemoryAllMissions.java
@@ -1,13 +1,26 @@
 package drones.gr1;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class InMemoryAllMissions implements AllMissions {
+    Set<Mission> missions = new HashSet<>();
+
     @Override
     public void add(Mission mission) {
-
+        if (!missions.contains(mission)) {
+            missions.add(mission);
+        } else {
+            throw new IllegalArgumentException("Mission already exists");
+        }
     }
 
     @Override
     public void remove(Mission mission) {
-
+        if (missions.contains(mission)) {
+            missions.remove(mission);
+        } else {
+            throw new IllegalArgumentException("Mission does not exist");
+        }
     }
 }
