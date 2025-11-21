01984b6440472b776d1209b315961baea6bdec26
Julien <102910533+Skyline75@users.noreply.github.com>
2025-11-21 10:51:31 +0100

    Add files via upload

---
+++ src/main/java/drones/gr1/Fleet.java
@@ -0,0 +1,25 @@
+package drones.gr1;
+
+public class Fleet {
+
+
+
+
+    // On peut supprimer si il n'est pas en MISSION
+    public Boolean RemoveDrone()
+    {
+
+    }
+
+    
+    public Boolean AddDrone()
+    {
+
+    }
+
+
+    //On peut ajouter une nouvelle mission à condition qu'un drone soit disponible (i.e. qu'il ne soit pas sur une autre mission et qu'il puisse atteindre toutes les positions en vérifiant les altitudes)
+    public Boolean AddMission() {
+
+    }
+}
